bd1ad6bd771e941ad05879da5637bcd13910e8e7
SunDoge <384813529@qq.com>
2017-05-21 02:55:52 +0800

    add migrate.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#composer install
-#php artisan key:generate
-#php artisan migrate
-php artisan migrate:refresh
+composer install
+php artisan key:generate
+php artisan migrate
 php artisan db:seed
