74b8e67f8108f036aac85d758ef48ad02143aa19
SunDoge <384813529@qq.com>
2017-05-27 00:16:55 +0800

    js

--- public/js/app.js
+++ public/js/app.js
@@ -31656,7 +31656,7 @@ var Component = __webpack_require__(35)(
   /* cssModules */
   null
 )
-Component.options.__file = "/home/sundoge/Sites/uml-homework/resources/assets/js/components/cinema/Movie.vue"
+Component.options.__file = "/Users/sundoge/Sites/cinema/resources/assets/js/components/cinema/Movie.vue"
 if (Component.esModule && Object.keys(Component.esModule).some(function (key) {return key !== "default" && key !== "__esModule"})) {console.error("named exports are not supported in *.vue files.")}
 if (Component.options.functional) {console.error("[vue-loader] Movie.vue: functional components are not supported with templates, they should use render functions.")}
 
@@ -31667,9 +31667,9 @@ if (false) {(function () {
   if (!hotAPI.compatible) return
   module.hot.accept()
   if (!module.hot.data) {
-    hotAPI.createRecord("data-v-7b27fbef", Component.options)
+    hotAPI.createRecord("data-v-4c16ce17", Component.options)
   } else {
-    hotAPI.reload("data-v-7b27fbef", Component.options)
+    hotAPI.reload("data-v-4c16ce17", Component.options)
   }
 })()}
 
@@ -31746,7 +31746,7 @@ module.exports.render._withStripped = true
 if (false) {
   module.hot.accept()
   if (module.hot.data) {
-     require("vue-hot-reload-api").rerender("data-v-7b27fbef", module.exports)
+     require("vue-hot-reload-api").rerender("data-v-4c16ce17", module.exports)
   }
 }
 
