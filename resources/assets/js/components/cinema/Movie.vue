8ba228d6ea88f701da334c3e495ff98b93932f98
SunDoge <384813529@qq.com>
2017-05-30 00:55:16 +0800

    大小调好了

--- resources/assets/js/components/cinema/Movie.vue
+++ resources/assets/js/components/cinema/Movie.vue
@@ -2,7 +2,7 @@
     <div class="box" @click="directToMovie">
         <article class="media">
             <div class="media-left">
-                <figure class="image">
+                <figure class="image is-128x128">
                     <img src="/img/seal.jpg" alt="post">
                 </figure>
             </div>
@@ -13,7 +13,21 @@
                     <p>Price: {{ movie.price }}元 Runtime: {{ movie.runtime }}分钟</p>
                     <p>Summary: {{ movie.summary }}</p>
                 </div>
+                <nav class="level is-mobile">
+                <div class="level-left">
+                    <a class="level-item">
+                        <span class="icon is-small"><i class="fa fa-reply"></i></span>
+                    </a>
+                    <a class="level-item">
+                        <span class="icon is-small"><i class="fa fa-retweet"></i></span>
+                    </a>
+                    <a class="level-item">
+                        <span class="icon is-small"><i class="fa fa-heart"></i></span>
+                    </a>
+                </div>
+            </nav>
             </div>
+
         </article>
 
     </div>
